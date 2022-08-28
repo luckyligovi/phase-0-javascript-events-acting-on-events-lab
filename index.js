1a10a3951b8e4c47c5d1b47ba61267a659f5897f
Lucky Ligovi <luckyligovi@gmail.com>
2022-08-29 02:45:20 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+
+function moveDodgerLeft(){
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+};
+
+function moveDodgerRight() {
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
